f7ac0b852cade5e66a6b2f1a5a874a33f7042b86
Luis Jarosz <luisito.lj@gmail.com>
2014-05-22 09:14:53 +0200

    Comienzo con la DB
    Ya era hora de ponerse a vincular las bases de datos.

--- index.php
+++ index.php
@@ -17,22 +17,37 @@
     <body background="img\sky_1.jpg" style="background-attachment: fixed">
     
         <h1 align="center">Bienvenido a Eartclooks</h1>
+        <!--El formulario funciona, pero habrá que ponerle algunas otras cosas con javascript o con html-->
+        <form name="contacto" action="index.php" method="POST">
+            <table id="tabla1">
+                <tr>
+                    <td><input id="nombre" type="text" name="nombre" placeholder="Usuario"/></td>
+                </tr>
+                <tr>
+                    <td><input id="pass" type="password" name="pass" placeholder="Contraseña"/></td>
+                </tr>
+                <tr>
+                    <td align="center"><input id="boton" name="boton" type="submit" value="Entrar"/></td>
+                </tr>
+                <tr>
+                    <td align="center"><a href="inscribe.php"><br>Darse de alta</a></td>
+                </tr>
+            </table>
+        </form>
+        <?php
+        /*Hay que vincular la base de datos para que los compare desde el propio formulario*/
+        $conexion = mysql_connect("localhost","root","") or die("No se ha podido conectar");
+        mysql_select_db("eartclooks", $conexion);
         
-        <table id="tabla1">
-            <tr>
-                <td><input id="nombre" type="text" name="nombre" placeholder="Usuario"/></td>
-            </tr>
-            <tr>
-                <td><input id="pass" type="password" name="pass" placeholder="Contraseña"/></td>
-            </tr>
-            <tr>
-                <td align="center"><input id="boton" name="boton" type="submit" value="Entrar"/></td>
-            </tr>
-            <tr>
-                <td align="center"><a href="inscribe.php"><br>Darse de alta</a></td>
-            </tr>
-         </table>
-             
+        $postUsuario = $_POST['nombre'];
+        $postContrasena = $_POST['pass'];
+        
+        if($postUsuario == 'Luis' && $postContrasena == 'Luis'){
+            echo '<script type="text/javascript">window.location="inicio.php";</script>';
+            die();
+        }
+        ?>
+        
         <script src="js/jquery-1.9.1.js"></script>
         <script src="js/jquery-ui-1.10.3.custom.min.js"></script>  
         <script src="js/bootstrap.min.js"></script>
--- inscribe.php
+++ inscribe.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -13,8 +14,53 @@
         <link rel="stylesheet" href="css/custom-theme/jquery-ui-1.10.3.custom.min.css">
     </head>
     <body background="img\fondo_1.jpg" style=" background-attachment: fixed">
-        
+        <div id="contenedor">
+            <div id="cabecera">
+                <center>
+                <img src="img/eartclooks.png" id="eartclooks">
+                </center>
+            </div><!--Fin de la cabecera-->
+            <div id="formulario">
+                    <form>
+                        <fieldset>
+                            <center>
+                            <table id="formTable">
+                                <tr>
+                                    <td>Nombre:</td>    <td><input type="text" name="nombre"></td>
+                                </tr>
+                                <tr>
+                                    <td>Apellido:</td>    <td><input type="text" name="apellido"></td>
+                                </tr>
+                                <tr>
+                                    <td>e-Mail:</td>    <td><input type="text" name="email"></td>
+                                </tr>
+                                <tr>
+                                    <td>Edad:</td>    <td><input type="text" name="edad"></td>
+                                </tr>
+                                <tr>
+                                    <td>Contraseña:</td>    <td><input type="password" name="pass1"></td>
+                                </tr>
+                                <tr>
+                                    <td>Repite contraseña:</td>    <td><input type="password" name="pass2"></td>
+                                </tr>
+                            </table>
+                            </center>
+                        </fieldset>
+                    </form>
+                <center>
+                <input id="boton" name="boton" type="submit" value="Suscribirse"/>
+                <br>
+                <br>
+                <a href="index.php">Volver a la página de inicio</a>
+                <br>
+                <br>
+                </center>
+                </div><!--Fin del formulario-->
+        </div><!--Fin del contenedor-->
         <?php
+        $conexion = mysql_connect("localhost","root","") or die("No se ha podido conectar");
+        mysql_select_db("eartclooks", $conexion);
+        
         
         ?>
         
